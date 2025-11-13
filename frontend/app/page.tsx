930129d6485b4fb22e1f6423fdf362cda9a5f034
TheChirag356 <arorachirag12005@gmail.com>
2025-11-13 18:04:02 +0530

    Basic application

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,65 +1,49 @@
+import Navbar from "@/components/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="flex min-h-screen flex-col items-center justify-center relative overflow-hidden">
+      <Navbar />
+      {/* Hero Section */}
+      <div className="relative flex flex-col items-center justify-center h-screen w-full text-center px-4 bg-foreground overflow-hidden">
+        <div className="absolute inset-0 -z-10">
+          <Image
+            src="/bg.jpg"
+            alt="background"
+            fill
+            priority
+            className="object-cover opacity-30 blur-[2px]"
+          />
+          <div className="absolute inset-0 bg-gradient-radial from-blue-900/20 via-purple-900/10 to-black mix-blend-overlay" />
+        </div>
+
+        <div className="max-w-4xl flex flex-col items-center justify-center">
+          <h1 className="text-5xl font-weird text-white">Start creating.</h1>
+          <p>Transform Hours of Footage into Minutes of Magic.</p>
+        </div>
+      </div>
+
+      <div className="h-screen max-w-5xl flex flex-col items-center justify-start px-4 text-center">
+        <div className="flex flex-col justify-center items-center mt-32 gap-4">
+          <h1 className="font-serif text-7xl">
+            Create short videos from long ones in a single click
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="text-muted-foreground font-sans">
+            A tool that can create short videos from long ones before you can
+            say <span className="font-weird">Adabra Kadabra</span>
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="h-screen w-full max-w-5xl">
+          <h1 className="font-serif text-7xl">
+            Built to work the way humans do
+          </h1>
+          <p className="text-muted-foreground font-sans">
+            Autocut's multi-pass system utilizes Whisper model and Diarization
+            for <span className="font-weird">seamless</span> video editing.
+          </p>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
